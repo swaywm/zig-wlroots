fc702780f6b27ff3bc4e99d359d73dd9bd3805e8
Isaac Freund <ifreund@ifreund.xyz>
2020-10-12 13:58:02 +0200

    Bind wlr_compositor

---
+++ src/compositor.zig
@@ -0,0 +1,25 @@
+const wlr = @import("wlroots.zig");
+
+const wayland = @import("wayland");
+const wl = wayland.server.wl;
+
+pub const Subcompositor = extern struct {
+    global: *wl.Global,
+};
+
+pub const Compositor = extern struct {
+    global: *wl.Global,
+    renderer: *wlr.Renderer,
+
+    subcompositor: Subcompositor,
+
+    display_destroy: wl.Listener,
+
+    events: extern struct {
+        new_surface: wl.Signal,
+        destroy: wl.Signal,
+    },
+
+    extern fn wlr_compositor_create(server: *wl.Server, renderer: *wlr.Renderer) ?*Compositor;
+    pub const create = wlr_compositor_create;
+};
--- wlroots.zig
+++ wlroots.zig
@@ -1,3 +1,8 @@
 const Backend = @import("src/backend.zig").Backend;
+
 const Renderer = @import("src/renderer.zig").Renderer;
+
 const Egl = @import("src/egl.zig").Egl;
+
+const Compositor = @import("src/compositor.zig").Compositor;
+const Subcompositor = @import("src/compositor.zig").Subcompositor;
