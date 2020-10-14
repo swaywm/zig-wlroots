a929a332674ec5811f4c7e363d75c0e7d3e9d4d3
Isaac Freund <ifreund@ifreund.xyz>
2020-10-14 18:37:06 +0200

    Bind wlr_input_device

---
+++ src/types/input_device.zig
@@ -0,0 +1,52 @@
+const wlr = @import("../wlroots.zig");
+
+const wayland = @import("wayland");
+const wl = wayland.server.wl;
+
+pub const ButtonState = extern enum {
+    pressed,
+    released,
+};
+
+pub const InputDevice = extern struct {
+    pub const Type = extern enum {
+        Keyboard,
+        Pointer,
+        Touch,
+        TabletTool,
+        TabletPad,
+        Switch,
+    };
+
+    const Impl = opaque {};
+
+    impl: *const Impl,
+
+    kind: Type,
+    vendor: c_uint,
+    product: c_uint,
+    name: [*:0]u8,
+
+    width_mm: f64,
+    height_mm: f64,
+    output_name: [*:0]u8,
+
+    /// InputDevice.kind determines which of these is active
+    device: extern union {
+        _device: ?*c_void,
+        keyboard: *wlr.Keyboard,
+        pointer: *wlr.Pointer,
+        switch_device: *wlr.Switch,
+        touch: *wlr.Touch,
+        tablet: *wlr.Tablet,
+        tablet_pad: *wlr.TabletPad,
+    },
+
+    events: extern struct {
+        destroy: wl.Signal,
+    },
+
+    data: ?*c_void,
+
+    link: wl.List,
+};
--- src/wlroots.zig
+++ src/wlroots.zig
@@ -6,9 +6,11 @@ pub const Renderer = @import("render/renderer.zig").Renderer;
 pub const Texture = @import("render/texture.zig").Texture;
 
 pub const Box = @import("types/box.zig").Box;
+pub const ButtonState = @import("types/input_device.zig").ButtonState;
 pub const Compositor = @import("types/compositor.zig").Compositor;
 pub const Cursor = @import("types/cursor.zig").Cursor;
 pub const DataDeviceManager = @import("types/data_device.zig").DataDeviceManager;
+pub const InputDevice = @import("types/input_device.zig").InputDevice;
 pub const Output = @import("types/output.zig").Output;
 pub const OutputLayout = @import("types/output_layout.zig").OutputLayout;
 pub const Subcompositor = @import("types/compositor.zig").Subcompositor;
