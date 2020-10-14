6aa98baca917ef4bb0c7ea28464642f862405a79
Isaac Freund <ifreund@ifreund.xyz>
2020-10-14 11:51:46 +0200

    Reorganize to mirror wlroots directory structure

---
+++ src/wlroots.zig
@@ -0,0 +1,15 @@
+pub const Backend = @import("backend.zig").Backend;
+
+pub const DmabufAttributes = @import("render/dmabuf.zig").DmabufAttributes;
+pub const Egl = @import("render/egl.zig").Egl;
+pub const Renderer = @import("render/renderer.zig").Renderer;
+pub const Texture = @import("render/texture.zig").Texture;
+
+pub const Box = @import("types/box.zig").Box;
+pub const Compositor = @import("types/compositor.zig").Compositor;
+pub const DataDeviceManager = @import("types/data_device.zig").DataDeviceManager;
+pub const Output = @import("types/output.zig").Output;
+pub const OutputLayout = @import("types/output_layout.zig").OutputLayout;
+pub const Subcompositor = @import("types/compositor.zig").Subcompositor;
+pub const Subsurface = @import("types/surface.zig").Subsurface;
+pub const Surface = @import("types/surface.zig").Surface;
