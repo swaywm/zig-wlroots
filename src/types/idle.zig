35a3cf990344df2f114cf0a83c668b9b9ed5e97c
Isaac Freund <ifreund@ifreund.xyz>
2020-11-03 17:14:51 +0100

    Bind wlr_idle.h

---
+++ src/types/idle.zig
@@ -0,0 +1,57 @@
+const wlr = @import("wlroots");
+
+const wayland = @import("wayland");
+const wl = wayland.server.wl;
+
+const Idle = extern struct {
+    global: *wl.Global,
+    /// IdleTimeout.link
+    idle_timers: wl.List,
+    event_loop: *wl.EventLoop,
+    enabled: bool,
+
+    server_destroy: wl.Listener(*wl.Server),
+    events: extern struct {
+        activity_notify: wl.Signal(*wlr.Seat),
+        destroy: wl.Signal(*Idle),
+    },
+
+    data: ?*c_void,
+
+    extern fn wlr_idle_create(server: *wl.Server) ?*Idle;
+    pub const create = wlr_idle_create;
+
+    extern fn wlr_idle_notify_activity(idle: *Idle, seat: *wlr.Seat) void;
+    pub const notifyActivity = wlr_idle_notify_activity;
+
+    extern fn wlr_idle_set_enabled(idle: *Idle, seat: ?*wlr.Seat, enabled: bool) void;
+    pub const setEnabled = wlr_idle_set_enabled;
+};
+
+const IdleTimeout = extern struct {
+    resource: *wl.Resource,
+    link: wl.List,
+    seat: *wlr.Seat,
+
+    idle_source: *wl.EventSource,
+    idle_state: bool,
+    enabled: bool,
+    timeout: u32,
+
+    events: extern struct {
+        idle: wl.Signal(*IdleTimeout),
+        resumed: wl.Signal(*IdleTimeout),
+        destroy: wl.Signal(void),
+    },
+
+    input_listener: wl.Listener(*wlr.Seat),
+    seat_destroy: wl.Listener(*wlr.Seat),
+
+    data: ?*c_void,
+
+    extern fn wlr_idle_timeout_create(idle: *Idle, seat: *wlr.Seat, timeout: u32) ?*IdleTimeout;
+    pub const create = wlr_idle_timeout_create;
+
+    extern fn wlr_idle_timeout_destroy(timeout: *IdleTimeout) void;
+    pub const destroy = wlr_idle_timeout_destroy;
+};
--- src/wlroots.zig
+++ src/wlroots.zig
@@ -41,6 +41,9 @@ pub const Touch = @import("types/touch.zig").Touch;
 pub const Tablet = @import("types/tablet_tool.zig").Tablet;
 pub const TabletTool = @import("types/tablet_tool.zig").TabletTool;
 
+pub const Idle = @import("types/idle.zig").Idle;
+pub const IdleTimeout = @import("types/idle.zig").IdleTimeout;
+
 pub const DataDeviceManager = @import("types/data_device.zig").DataDeviceManager;
 pub const DataOffer = @import("types/data_device.zig").DataOffer;
 pub const DataSource = @import("types/data_device.zig").DataSource;
