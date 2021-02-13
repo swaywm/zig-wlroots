9c915d649134308eb64a7228b2046be44093b0e3
Bonicgamer <bonicgamer55@gmail.com>
2021-02-13 14:05:30 -0500

    Suggested changes

--- src/util/region.zig
+++ src/util/region.zig
@@ -1,6 +1,7 @@
 const pixman = @import("pixman");
 
-pub const Bool = c_int;
+const wayland = @import("wayland");
+const wl = wayland.server.wl;
 
 extern fn wlr_region_scale(dst: *pixman.Region32, src: *pixman.Region32, scale: f32) void;
 pub const scale = wlr_region_scale;
@@ -17,5 +18,5 @@ pub const expand = wlr_region_expand;
 extern fn wlr_region_rotated_bounds(dst: *pixman.Region32, src: *pixman.Region32, rotation: f32, ox: c_int, oy: c_int) void;
 pub const rotatedBounds = wlr_region_rotated_bounds;
 
-extern fn wlr_region_confine(region: *pixman.Region32, x1: f64, y1: f64, x2: f64, y2: f64, x2_out: *f64, y2_out: *f64) Bool;
+extern fn wlr_region_confine(region: *pixman.Region32, x1: f64, y1: f64, x2: f64, y2: f64, x2_out: *f64, y2_out: *f64) bool;
 pub const confine = wlr_region_confine;
